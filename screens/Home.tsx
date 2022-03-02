492d9485f7f3e254eb7a2f6b1d1e94538f944ce5
Rabiaparlak <68352029+Rabiaparlak@users.noreply.github.com>
2022-03-03 01:05:34 +0300

    container part is done. Style file edited.
    container part is done. Style file edited.

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,37 +1,67 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { View, TouchableOpacity, Text,Image,TextInput  } from "react-native";
+import { View, TouchableOpacity, Text, Image, TextInput } from "react-native";
 import { styles } from "./style";
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, FontAwesome } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
 
 
 const Home = () => {
 
 
-    const navigation: any = useNavigation();
+  const navigation: any = useNavigation();
 
-    return (
-      
-        <View style={styles.body}>
-          <StatusBar hidden />
-          <View style={styles.header}>
-            <View style={styles.headerTitleArea}>
-              <Text style={styles.headerTitle}>Hello Daria.</Text>
-              <Ionicons style={styles.headerIcon} name="md-menu-outline" size={40} color={"white"} />
-              <View style={styles.profileArea}>
-              <Image source={require('./img/pp.jpg')} style={styles.profile}/>
-              </View>
-            </View>
-            <View style={styles.searchArea}>
-              <Ionicons style={styles.searchIcon} name="search-outline" size={27} color={"white"} />
-              <TextInput placeholder="What are you looking for?" style={styles.searchText}>
-              </TextInput>
+  return (
+
+    <View style={styles.body}>
+      {/* telefonun durum çubuğunu kaldırmak için  */}
+      <StatusBar hidden />
+      {/* sayfanın header kısmı  */}
+      <View style={styles.header}>
+        <View style={styles.headerTitleArea}>
+          <Text style={styles.headerTitle}>Hello Daria.</Text>
+          <Ionicons style={styles.headerIcon} name="md-menu-outline" size={40} color={"white"} />
+          <View style={styles.profileArea}>
+            <Image source={require('./img/pp.jpg')} style={styles.profile} />
+          </View>
+        </View>
+        <View style={styles.searchArea}>
+          <Ionicons style={styles.searchIcon} name="search-outline" size={27} color={"white"} />
+          <TextInput placeholder="What are you looking for?" placeholderTextColor="#D2CDEF" style={styles.searchText}>
+          </TextInput>
+        </View>
+      </View>
+
+      {/* Container kısmı  */}
+
+      <View style={styles.container}>
+        <Text style={styles.title}>Popular in Warsaw</Text>
+        <View style={styles.products}>
+          <View style={styles.product}>
+            <Image source={require('./img/pp.jpg')} style={styles.productPhoto} />
+            <Text style={styles.productName}>Cute Cut-Hair salon</Text>
+            <Text style={styles.productDescription}>1.6 km away</Text>
+            <View style={styles.description}>
+              <FontAwesome name="star" size={16} color={"#75D1DD"} />
+              <Text style={styles.starText}>4.5/5</Text>
             </View>
           </View>
-          
-          
+
+          <View style={styles.product}>
+            <Image source={require('./img/pp.jpg')} style={styles.productPhoto} />
+            <Text style={styles.productName}>Cute Cut-Hair salon</Text>
+            <Text style={styles.productDescription}>1.6 km away</Text>
+            <View style={styles.description}>
+              <FontAwesome name="star" size={16} color={"#75D1DD"} />
+              <Text style={styles.starText}>4.5/5</Text>
+            </View>
+          </View>
+
         </View>
-    )
+      </View>
+
+    </View>
+
+  )
 }
 export default Home;
--- screens/style.tsx
+++ screens/style.tsx
@@ -9,18 +9,96 @@ import {
 
 export const styles = StyleSheet.create({
     
-body:{ flex: 1, alignItems: 'center', justifyContent: 'center' },
-header:{borderBottomLeftRadius:25,position:'absolute',top:0,backgroundColor:'#7A78D4',width:windowWidth ,height:windowHeight*0.25 ,padding: hp('5%') },
-headerTitleArea:{flexDirection:"row",marginTop:hp('2%')},
-headerTitle:{fontSize: wp('9%'),color:'white'},
-headerIcon:{marginLeft:wp('16%')},
-profileArea:{width:wp('12%'),height:hp('6%'),backgroundColor:'white',borderRadius:19,marginLeft:wp('4%')},
-profile:{width:wp('12%'),height:hp('6%'),borderRadius:19},
-searchArea:{flexDirection:"row",height:hp('7%'),backgroundColor:'rgba(255, 255, 255, 0.20)',marginTop:wp('5%'),borderRadius:18},
-searchIcon:{paddingTop:hp('1.5%'),paddingLeft:wp('3%')},
-searchText:{textAlign:'left',paddingLeft:wp('3%')},
+body:{ 
+  flex: 1, 
+  alignItems: 'flex-end', 
+  justifyContent: 'flex-end' 
+},
+header:{
+  borderBottomLeftRadius:25,
+  position:'absolute',
+  top:0,
+  backgroundColor:'#7A78D4',
+  width:windowWidth ,
+  height:windowHeight*0.25 ,
+  padding: hp('5%') 
+},
+headerTitleArea:{
+  flexDirection:"row",
+  marginTop:hp('2%')
+},
+headerTitle:{
+  fontSize: wp('9%'),
+  color:'white'
+},
+headerIcon:{
+  marginLeft:wp('16%')
+},
+profileArea:{
+  width:wp('12%'),
+  height:hp('6%'),
+  backgroundColor:'white',
+  borderRadius:19,
+  marginLeft:wp('4%')
+},
+profile:{
+  width:wp('12%'),
+  height:hp('6%'),
+  borderRadius:19
+},
+searchArea:{
+  flexDirection:"row",
+  height:hp('7%'),
+  backgroundColor:'rgba(255, 255, 255, 0.20)',
+  marginTop:wp('5%'),
+  borderRadius:18
+},
+searchIcon:{
+  paddingTop:hp('1.5%'),
+  paddingLeft:wp('3%')
+},
+searchText:{
+  textAlign:'left',
+  paddingLeft:wp('3%'),
+  color:'#fff'
+},
+container:{ 
+  width:windowWidth,
+  height:windowHeight*0.81,
+  paddingLeft:wp('2%')
+},
+title:{
+  fontSize:wp('7%'),
+  color:'#59585F',
+  textAlign:'left',
+  marginTop:hp('3%'),
+  marginLeft:wp('8%')
+},
 
+products:{flexDirection:"row"},
 
-
-
+product:{marginLeft:wp('8%')},
+productPhoto:{
+  width:windowWidth*0.5,
+  height:windowHeight*0.2,
+  marginTop:hp('3%'),
+  borderRadius:20
+},
+productName:{
+  color:'#59585F',
+  fontSize:wp('5%'),
+  marginTop:hp('2%')
+},
+description:{
+  flexDirection:"row",
+  marginTop:hp('2.2%')
+},
+productDescription:{
+  color:'#C0C0C2',
+  marginTop:hp('1%')
+},
+starText:{
+  color:'#59585F',
+  marginLeft:wp('1%')
+}
 })
